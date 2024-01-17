0e6053a8871602e9bcc711c9b74d1a06a78a1818
choptop84 <monecraftwithmike123@gmail.com>
2024-01-16 21:47:38 -0500

    torture machine is actually really good

--- website/beepbox_editor.js
+++ website/beepbox_editor.js
@@ -31255,7 +31255,7 @@ You should be redirected to the song at:<br /><br />
 				.beepboxEditor {
 					width: 100%;
 					height: 100vh;
-					grid-template-columns: minmax(0, 1fr) 235px; 
+					grid-template-columns: minmax(0, 1fr) 190px; 
 					grid-template-rows: minmax(481px, 1fr) minmax(0, min-content);
 					grid-template-areas: "pattern-area settings-area" "track-area";
 				}
