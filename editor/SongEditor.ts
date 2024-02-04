4d6004a3d99675e2d5bf8cb2a39bfd726b6fc4cd
choptop84 <monecraftwithmike123@gmail.com>
2024-02-03 20:58:39 -0500

    Revert "Zoom Buttons in Mobile Discord"
    This reverts commit b7090da5ef00cd2b62a4f5d8a04c5767bd0e9f14.

--- editor/SongEditor.ts
+++ editor/SongEditor.ts
@@ -2274,6 +2274,8 @@ export class SongEditor {
                 this._patternEditor.container.style.flexShrink = "";
                 this._patternEditorPrev.container.style.display = "none";
                 this._patternEditorNext.container.style.display = "none";
+                this._zoomInButton.style.display = "none";
+                this._zoomOutButton.style.display = "none";
                 this._undoButton.style.display = (this._doc.channel < this._doc.song.pitchChannelCount) ? "" : "none";
                 this._redoButton.style.display = (this._doc.channel < this._doc.song.pitchChannelCount) ? "" : "none";
                 this._copyPatternButton.style.display = (this._doc.channel < this._doc.song.pitchChannelCount) ? "" : "none";
@@ -2309,10 +2311,6 @@ export class SongEditor {
                 this._loopBarButton.style.left = prefs.showScrollBar ? "2px" : "2px";
                 this._fullscreenButton.style.top = prefs.showScrollBar ? "330px" : "330px";
                 this._fullscreenButton.style.left = prefs.showScrollBar ? "2px" : "2px";
-                this._zoomInButton.style.top = prefs.showScrollBar ? "360px" : "360px";
-                this._zoomInButton.style.left = prefs.showScrollBar ? "2px" : "2px";
-                this._zoomOutButton.style.top = prefs.showScrollBar ? "390px" : "390px";
-                this._zoomOutButton.style.left = prefs.showScrollBar ? "2px" : "2px";
                 this._patternArea.style.paddingLeft = prefs.showScrollBar ? "32px" : "32px" ;
                 this._patternArea.style.maxHeight =  "75vh";
                 this._patternArea.style.height =  "481px";
