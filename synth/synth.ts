6bbaaf41c2d181f43cd372e29add899ef04e4388
choptop84 <monecraftwithmike123@gmail.com>
2025-06-26 23:40:42 -0400

    Fixed a crash caused by unison buzzing

--- synth/synth.ts
+++ synth/synth.ts
@@ -4998,7 +4998,9 @@ export class Song {
                     instrument.unisonSign = unisonSign / 1000;
                     if (unisonSignNegative == 0) instrument.unisonSign *= -1;
 
-                    instrument.unisonBuzzes = base64CharCodeToInt[compressed.charCodeAt(charIndex++)] ? true : false;
+                    if ((fromUltraBox && !beforeSix) || (fromAbyssBox && !beforeThree)) {
+                        instrument.unisonBuzzes = base64CharCodeToInt[compressed.charCodeAt(charIndex++)] ? true : false;
+                    }
                 } else {
                     instrument.unisonVoices = Config.unisons[instrument.unison].voices;
                     instrument.unisonSpread = Config.unisons[instrument.unison].spread;
